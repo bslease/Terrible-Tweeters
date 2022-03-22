91c8896d6c64d7b9c330107d9e55e45609a1814b
bslease <bslease@gmail.com>
2022-03-22 16:06:00 -0700

    add crates and monsters

--- Terrible Tweeters Project/Assets/Scripts/Bird.cs
+++ Terrible Tweeters Project/Assets/Scripts/Bird.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -50,5 +51,19 @@ public class Bird : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ResetAfterDelay());
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+
+        _rigidbody2D.position = _startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
     }
 }
---
+++ Terrible Tweeters Project/Assets/Scripts/Monster.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (ShouldDieFromCollision(collision))
+        {
+            Die();
+        }
+    }
+
+    private bool ShouldDieFromCollision(Collision2D collision)
+    {
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+        if (bird != null)
+            return true;
+
+        if (collision.contacts[0].normal.y < -0.5)
+            return true;
+
+        return false;
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
